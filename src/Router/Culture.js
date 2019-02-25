1df5074d23691e64b0fcacc468dd57fc1f8f5550
souzolliveira <emanueloliveira38@yahoo.com.br>
2019-02-25 16:23:22 -0300

    puxando culturas dinamicamente

--- src/Router/Culture.js
+++ src/Router/Culture.js
@@ -5,87 +5,47 @@ export default class Culture extends Component{
     constructor(){
         super();
 
+        this.CultureList = this.CultureList.bind(this);
+        
         this.state = {
-            culturas: [],
-            length: 0,
+            culturas: [{
+                id: 0,
+                cultura: '',
+            }],
         }
     }
-    render(){
+    CultureList(){
         axios.get('https://api.cnptia.embrapa.br/agritec/v1/culturas', {headers: {'Authorization': 'Bearer d4c07cde-dacc-3194-a535-37300f024951'}})
           .then(response => {
             this.setState(()=>{
                 return {                        
                     culturas: response.data.data,
-                    length: response.data.data.length,
                 }
-          })         
+          })
         })
-
-        const culturas = this.state.culturas;
-
-        for (var i = 0; i < this.state.length; i += 1) {
-            culturas.push(<CultureComponent value={this.state.culturas.id} nome={this.state.culturas.cultura}/>);
-        };
-
-        console.log(this.state.culturas);
+    }
+    render(){
+        let cult = this.state.culturas;
+        let optionItems = cult.map((data) =>
+            <div className="container">
+                <ul className="list">
+                    <li className="list__item">
+                        <input type="radio" className="radio-btn" name="choice" id={data.id} />
+                        <label for={data.id} className="label">{data.cultura}</label>
+                    </li>
+                </ul>
+            </div>
+        );
 
         return (
-            <div className='content'>
+            <div className='content' onClick={this.CultureList}>
                 <h2>Cultura</h2>
                 <p> selecione a cultura desejada </p>
-                <div className='options'>
-                    <div className="container">
-                    <ul className="list">
-                        <li className="list__item">
-                        <input type="radio" className="radio-btn" name="choice" id="opt-0" />
-                        <label for="opt-0" className="label">ARROZ</label>
-                        </li>
-                    </ul>
-                    </div>
-                    <div className="container">
-                    <ul className="list">
-                        <li className="list__item">
-                        <input type="radio" className="radio-btn" name="choice" id="opt-1" />
-                        <label for="opt-1" className="label">FEIJÃO</label>
-                        </li>
-                    </ul>
-                    </div>
-                    <div className="container">
-                    <ul className="list">
-                        <li className="list__item">
-                        <input type="radio" className="radio-btn" name="choice" id="opt-2" />
-                        <label for="opt-2" className="label">MILHO</label>
-                        </li>
-                    </ul>
-                    </div>
-                    <div className="container">
-                    <ul className="list">
-                        <li className="list__item">
-                        <input type="radio" className="radio-btn" name="choice" id="opt-3" />
-                        <label for="opt-3" className="label">SOJA</label>
-                        </li>
-                    </ul>
-                    </div>
-                    <div className="container">
-                    <ul className="list">
-                        <li className="list__item">
-                        <input type="radio" className="radio-btn" name="choice" id="opt-4" />
-                        <label for="opt-4" className="label">TRIGO</label>
-                        </li>
-                    </ul>
-                    </div>
+                <div className='options' >                
+                    {optionItems}
                 </div>
             </div>
         );
     }
 }
-const CultureComponent = props => 
-    <div className="container">
-        <ul className="list">
-            <li className="list__item">
-                <input type="radio" className="radio-btn" name="choice" id={props.value} />
-                <label for={props.value} className="label">{props.nome}</label>
-            </li>
-        </ul>
-    </div>;
   
