10e37f755a7f19689a45448de95aabfbe9d9b75b
souzolliveira <emanueloliveira38@yahoo.com.br>
2019-04-12 15:03:35 -0300

    Updates

--- precache-manifest.9b9914a48dce4a0fc322cd084b60cda8.js
+++ precache-manifest.9b9914a48dce4a0fc322cd084b60cda8.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/primeiro-app/static/js/runtime~main.e345aeee.js"
   },
   {
-    "revision": "25ad803ddf1b641416f0",
-    "url": "/primeiro-app/static/js/main.25ad803d.chunk.js"
+    "revision": "2c020357de31d830c0ea",
+    "url": "/primeiro-app/static/js/main.2c020357.chunk.js"
   },
   {
     "revision": "ffb7420b6ca105cb37cc",
     "url": "/primeiro-app/static/js/1.ffb7420b.chunk.js"
   },
   {
-    "revision": "25ad803ddf1b641416f0",
+    "revision": "2c020357de31d830c0ea",
     "url": "/primeiro-app/static/css/main.a8b7be6d.chunk.css"
   },
   {
-    "revision": "3e322e6643c0156040551896c6647838",
+    "revision": "e9b9347ec2e98f17047ae2278a947761",
     "url": "/primeiro-app/index.html"
   }
 ];
