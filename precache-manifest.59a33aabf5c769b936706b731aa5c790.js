e54fe5cac0f13165b20678b21e59c48bae339997
souzolliveira <emanueloliveira38@yahoo.com.br>
2019-04-22 15:36:55 -0300

    Updates

--- precache-manifest.59a33aabf5c769b936706b731aa5c790.js
+++ precache-manifest.59a33aabf5c769b936706b731aa5c790.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/primeiro-app/static/js/runtime~main.e345aeee.js"
   },
   {
-    "revision": "8d562a74ddac7aec6d03",
-    "url": "/primeiro-app/static/js/main.8d562a74.chunk.js"
+    "revision": "7aba5970efded26a0605",
+    "url": "/primeiro-app/static/js/main.7aba5970.chunk.js"
   },
   {
     "revision": "32a9222ea73df40cd9e6",
     "url": "/primeiro-app/static/js/1.32a9222e.chunk.js"
   },
   {
-    "revision": "8d562a74ddac7aec6d03",
+    "revision": "7aba5970efded26a0605",
     "url": "/primeiro-app/static/css/main.76607cb9.chunk.css"
   },
   {
-    "revision": "bb741c91a44bbd0441ecce125341703c",
+    "revision": "8c18421ec127e9eeb2b29919c66c377a",
     "url": "/primeiro-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["static/css/main.76607cb9.chunk.css","7c3c49ddddefc6f00dbcda2dd1beebf5"],["static/js/1.32a9222e.chunk.js","eea15fb8e754e70f4dbfcc4c8ad24aeb"],["static/js/main.8d562a74.chunk.js","db07abdb26a635c1d91d3c2a26f4526c"],["static/js/runtime~main.e345aeee.js","04f0f48696059ec21a37f1e666695194"]];
+var precacheConfig = [["static/css/main.76607cb9.chunk.css","7c3c49ddddefc6f00dbcda2dd1beebf5"],["static/js/1.32a9222e.chunk.js","eea15fb8e754e70f4dbfcc4c8ad24aeb"],["static/js/main.7aba5970.chunk.js","ef67d6c240f018b4fcd266be20c3452c"],["static/js/runtime~main.e345aeee.js","04f0f48696059ec21a37f1e666695194"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
